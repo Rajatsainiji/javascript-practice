29cea366df24be55f53b9eea63fe0dff3de71361
Rajatsainiji <rajatsaini798317@gmail.com>
2026-06-11 21:57:11 +0530

    string question

---
+++ LogicDevelopment02/string09.js
@@ -0,0 +1,21 @@
+// Task 1
+// const name = "Rajat";
+// Print:
+// R
+// t
+// using indexes.
+
+const name = "Rajat";
+
+console.log(name[0])
+console.log(name[4])
+
+// Task 2
+// Count characters in:
+// "JavaScript"
+// Expected:
+// 10
+
+const str = "JavaScript";
+console.log(str.length)
+
